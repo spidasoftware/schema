bacb2a2b524cfc070d50e3544e6cc947cd3aae1f
Jeremy Wentworth <jeremy.wentworth@spidasoftware.com>
2013-11-12 15:05:59 -0500

    minor formatting

--- src/test/groovy/com/spidasoftware/schema/validation/LoggingTest.groovy
+++ src/test/groovy/com/spidasoftware/schema/validation/LoggingTest.groovy
@@ -2,20 +2,19 @@ package com.spidasoftware.schema.validation
 
 import org.apache.log4j.*
 
-class LoggingTest extends GroovyTestCase { 
+class LoggingTest extends GroovyTestCase {
 
 	def log = Logger.getLogger(this.class)
 
-  	void test(){
-  		def testLogMessage = "this text should appear in stdout and the log file"
+	void test(){
+		def testLogMessage = "this text should appear in stdout and the log file"
 		log.info(testLogMessage)
-		
-		def fileAppender = Logger.getRootLogger().getAppender("logfile");  
+
+		def fileAppender = Logger.getRootLogger().getAppender("logfile");
 		assert fileAppender
-		
+
 		def logfile = new File(fileAppender.getFile())
 		assert logfile.exists()
 		assert logfile.text.contains(testLogMessage)
 	}
-
 }
