39c0e044570a58b9085a8d23f3a7b161aecad12b
pfried <philip.fried@ijus.net>
2014-01-17 13:02:59 -0500

    added basic http client

--- src/main/groovy/com/spidasoftware/schema/client/GenericHttpClient.groovy
+++ src/main/groovy/com/spidasoftware/schema/client/GenericHttpClient.groovy
@@ -3,6 +3,7 @@ package com.spidasoftware.schema.client
 import com.spidasoftware.schema.utils.SSLUtils
 import groovy.util.logging.Log4j
 import org.apache.http.HttpEntity
+import org.apache.http.HttpEntityEnclosingRequest
 import org.apache.http.HttpResponse
 import org.apache.http.NameValuePair
 import org.apache.http.client.HttpClient
@@ -13,6 +14,8 @@ import org.apache.http.client.methods.HttpHead
 import org.apache.http.client.methods.HttpPost
 import org.apache.http.client.methods.HttpPut
 import org.apache.http.client.methods.HttpRequestBase
+import org.apache.http.client.methods.HttpUriRequest
+import org.apache.http.client.methods.RequestBuilder
 import org.apache.http.client.utils.URIBuilder
 import org.apache.http.entity.mime.MultipartEntityBuilder
 import org.apache.http.entity.mime.content.ContentBody
@@ -28,10 +31,14 @@ import org.apache.http.util.EntityUtils
 import java.nio.file.Files
 
 /**
- * Generic http client class for making most of the basic http calls. If you need to
- * do anything fancy, you may set a custom HttpClient, though it likely won't be necessary..
+ * Generic http client class for making most of the basic http calls. It is fairly customizable by overriding the
+ * Closures that it uses to create and configure the client and cleanup the responses and connections. The
+ * default implementation will automatically close the response output stream and release the connections after each call.
+ * It will not automatically cleanup the httpClient, however. You must call the shutdown() method when you are done in order
+ * avoid leaking connections.
+ *
  * <br>
- * By default, the client will follow redirects.
+ * By default, the client will follow redirects for GET and HEAD requests only.
  *
  * Created with IntelliJ IDEA.
  * User: pfried
@@ -40,188 +47,192 @@ import java.nio.file.Files
  * To change this template use File | Settings | File Templates.
  */
 @Log4j
-class GenericHttpClient {
-
-    /*
-     *The supported http methods are here for convenience
-     */
-    public static String POST = "post"
-    public static String GET = "get"
-    public static String DELETE = "delete"
-    public static String PUT = "put"
-    public static String HEAD = "head"
+class GenericHttpClient implements HttpClientInterface {
 
 
-    protected CloseableHttpClient client
+	CloseableHttpClient client
 
-    /**
-     * Closure for creating the HttpClient to be used for executing the requests.
-     * Default is a pretty standard implementation that will follow redirects on GET and HEAD requests,
-     * but not on anything else.
-     * If you create you own client, you may also need to customize the cleanupClient closure.
-     */
-    def createClient = {clientBuilder ->
-        clientBuilder.build()
-    }
+	/**
+	 * Executes the specified request with the given headers and parameters. After executing the request,
+	 * the responseHandler closure is called with the response, an then the response entity is consumed and
+	 * the request connection is released. All of this default behavior is defined in closures that can be
+	 * easily overridden for a particular instnace without having to define a new subclass.
+	 * For a POST or PUT request, all the parameters will be put into a multipart entity, including files.
+	 * For any other request type, the parameters will be added to the URI.
+	 *
+	 * @param httpMethod specify the http method to use: GET, POST, PUT, DELETE, etc.
+	 * @param uri base uri to make the request to, parameters will automatically be added if appropriate
+	 * @param parameters a map of params that will get added to the request Entity (for POST or PUT), or
+	 *      added to the URI (any other method). These will automatically be escaped as needed. may be null
+	 * @param headers a map of request headers to use for this request. May be null
+	 * @param responseHandler an optional Closure that get's called after the response has been received. The default will
+	 *      simply return the response body as a String, or throw an HttpResponseException if the status code is >= 300.
+	 *      Note that the default behavior is for GenericHttpClient to call consumeContent() on the entity AFTER this closure
+	 *      returns. It will then release the requests connection and then return the result of the responseHandler Closure.
+	 * @return
+	 */
+	def executeRequest(String httpMethod, URI uri, Map<String, Object> parameters, Map<String, String> headers, Closure responseHandler = getBodyAsString) {
+		def request = createRequest(httpMethod, uri, parameters, headers)
+		logRequest(request)
+		return executeHttpRequest(request, responseHandler)
 
-    /**
-     *
-     */
-    def cleanupClient = {client ->
-        if (client.respondsTo("close")) {
-            client.close()
-        }
-    }
+	}
+
+	def createRequest = {String httpMethod, URI uri, Map<String, Object> parameters, Map<String, String> headers ->
+		def requestBuilder = RequestBuilder.create(httpMethod.toUpperCase())
+		requestBuilder.setUri(uri)
+		if (headers && !headers.isEmpty()) {
+			headers.each { k, v ->
+				requestBuilder.addHeader(k, v)
+			}
+		}
+
+		if (parameters && !parameters.isEmpty()) {
+			boolean containsFile = parameters.values().any { it instanceof File }
+			boolean canContainFile = httpMethod.equalsIgnoreCase("POST") || httpMethod.equalsIgnoreCase("PUT")
+
+			// If there are files in the request, we'll have to build the entity ourselves
+			if (containsFile && canContainFile) {
+				requestBuilder.setEntity(createMultipartEntity(parameters))
+			} else {
+				parameters.each { k, v ->
+					requestBuilder.addParameter(k, v.toString())
+				}
+			}
+		}
+		return requestBuilder.build()
+	}
+
+	static void logRequest(HttpUriRequest request) {
+		log.info("Http Request: method: ${request.getMethod()}, URI: ${request.getURI().toString()}")
+		request.getAllHeaders().each {
+			log.debug("Request Header: ${it.name}: ${it.value}")
+		}
+
+	}
+
+	/**
+	 * Closure for creating the HttpClient to be used for executing the requests.
+	 * Default is a pretty standard implementation that will follow redirects on GET and HEAD requests,
+	 * but not on anything else.
+	 * If you create you own client, you may also need to customize the cleanupClient closure.
+	 */
+	def createClient = { clientBuilder ->
+		clientBuilder.build()
+	}
+
+	/**
+	 * call this when you are done!
+	 */
+	def cleanupClient = {
+		if (client.respondsTo("close")) {
+			client.close()
+		}
+	}
+
+	/**
+	 * default Closure for getting the body of an HttpResponse as a string. Can be passed into
+	 * other methods like:
+	 * <br><code>
+	 *     def responseString = new GenericHttpClient().get(new URI("http://www.spidamin.com/calcdb/projects"), [name: "projectName"], client.getBodyAsString)
+	 * </code>
+	 * This will return the response body as a string. If the response status code is >= 300, an HttpResponseException will be thrown.
+	 */
+	def getBodyAsString = { response ->
+		if (response.getStatusLine().getStatusCode() >= 300) {
+			log.error("Received error response from server. Code: ${response.getStatusLine().getStatusCode()}, Reason: ${response.getStatusLine().getReasonPhrase()}")
+			throw new HttpResponseException(response.getStatusLine().getStatusCode(), response.getStatusLine().reasonPhrase)
+		}
+		def body = EntityUtils.toString(response.getEntity())
+		return body
+	}
+
+	/**
+	 * Called by client.shutdown()
+	 * Closure for cleaning up an Http connection after the request has completed.
+	 * Default will release the connection every time.
+	 * If you're using a custom ConnectionManager, you may want to set your own cleanupRequest closure
+	 * to allow connections to remain open and be reused
+	 */
+	def cleanupRequest = { request ->
+		request.releaseConnection()
+
+	}
+
+	/**
+	 *  Closure for performing cleanup operations on an HttpRepsonse.
+	 *  Default will just close the inputStream, if it has one.
+	 */
+	def cleanupResponse = { response ->
+		response?.getEntity()?.getContent()?.close()
+	}
+
+	/**
+	 * Called when the protocol is https.
+	 */
+	def setupSSL = { request ->
+		SSLUtils.setupSSL(request.getURI().toURL())
+	}
 
 
-    /**
-     * default Closure for getting the body of an HttpResponse as a string. Can be passed into
-     * other methods like:
-     * <br><code>
-     *     def responseString = new GenericHttpClient().get(new URI("http://www.spidamin.com/calcdb/projects"), [name: "projectName"], client.getBodyAsString)
-     * </code>
-     * This will return the response body as a string. If the response status code is >= 300, an HttpResponseException will be thrown.
-     */
-    def getBodyAsString = {response ->
-        if (response.getStatusLine().getStatusCode() >= 300) {
-            log.error("Received error response from server. Code: ${response.getStatusLine().getStatusCode()}, Reason: ${response.getStatusLine().getReasonPhrase()}")
-            throw new HttpResponseException(response.getStatusLine().getStatusCode(), response.getStatusLine().reasonPhrase)
-        }
-        def body = EntityUtils.toString(response.getEntity())
-        return body
-    }
+	def executeHttpRequest(HttpUriRequest request, Closure responseHandler) throws Exception {
+		URI uri = request.getURI()
+		log.debug("Executing request: ${request.getMethod()} to URI: ${uri.toString()}")
 
-    /**
-     * Closure for cleaning up an Http connection after the request has completed.
-     * Default will release the connection every time.
-     * If you're using a custom ConnectionManager, you may want to set your own cleanupRequest closure
-     * to allow connections to remain open and be reused
-     */
-    def cleanupRequest = {request ->
-        request.releaseConnection()
+		if (!client) {
+			this.client = createClient(HttpClientBuilder.create())
+		}
 
-    }
+		if (uri.toURL().getProtocol() == "https") {
+			setupSSL(request)
+		}
 
-    /**
-     *  Closure for performing cleanup operations on an HttpRepsonse.
-     *  Default will just close the inputStream, if it has one.
-     */
-    def cleanupResponse = {response ->
-        response?.getEntity()?.getContent()?.close()
-    }
+		def response
+		def returnValue
+		Throwable error
+		try {
+			HttpContext localContext = new BasicHttpContext()
+			response = this.client.execute(request, localContext)
+			returnValue = responseHandler(response)
 
-    /**
-     * Called when the protocol is https.
-     */
-    def setupSSL = {request ->
-        SSLUtils.setupSSL(request.getURI().toURL())
-    }
+		} catch (Exception e) {
+			error = e
+			log.error(e, e)
+		} finally {
+			try {
+				if (response) {
+					cleanupResponse(response)
+				}
+				cleanupRequest(request)
+			} catch (Exception ex) {
+				log.debug(ex, ex)
+			}
+		}
 
-    def get(URI uri, Map<String, Object> parameters, Closure responseHandler = getBodyAsString){
-        URIBuilder builder = new URIBuilder(uri)
-        if (parameters && !parameters.isEmpty()) {
-            builder.setParameters(createBasicNameValuePairs(parameters))
-        }
-        HttpGet httpGet = new HttpGet(builder.build())
+		if (error) {
+			throw error
+		}
+		return returnValue
+	}
 
-        return executeHttpRequest(httpGet, responseHandler)
-    }
+	void shutdown(){
+		cleanupClient()
+	}
 
-    def put(URI uri, Map<String, Object> parameters, Closure responseHandler = getBodyAsString){
-        HttpPut put = new HttpPut(uri)
-        if (parameters && !parameters.isEmpty()) {
-            put.setEntity(createMultipartEntity(parameters))
-        }
-        return executeHttpRequest(put, responseHandler)
-    }
+	protected static HttpEntity createMultipartEntity(Map<String, Object> params) {
+		MultipartEntityBuilder builder = MultipartEntityBuilder.create()
+		params?.each { k, v ->
+			ContentBody bodyPart
+			if (v instanceof File) {
+				bodyPart = new FileBody(v, Files.probeContentType(v.toPath()), "UTF-8")
+			} else {
+				bodyPart = new StringBody(v.toString())
+			}
+			builder.addPart(k, bodyPart)
+		}
+		return builder.build()
 
-    def post(URI uri, Map<String, Object> parameters, Closure responseHandler = getBodyAsString) {
-        HttpPost post = new HttpPost(uri)
-        if (parameters && !parameters.isEmpty()) {
-            post.setEntity(createMultipartEntity(parameters))
-        }
-        return executeHttpRequest(post, responseHandler)
-    }
+	}
 
-    def delete(URI uri, Map<String, Object> parameters, Closure responseHandler = getBodyAsString){
-        URIBuilder builder = new URIBuilder(uri)
-        if (parameters && !parameters.isEmpty()) {
-            builder.addParameters(createBasicNameValuePairs(parameters))
-        }
-        HttpDelete delete = new HttpDelete(builder.build())
-        return executeHttpRequest(delete, responseHandler)
-    }
-
-    def head(URI uri, Map<String, Object> parameters, Closure responseHandler = getBodyAsString){
-        URIBuilder builder = new URIBuilder(uri)
-        if (parameters && !parameters.isEmpty()) {
-            builder.addParameters(createBasicNameValuePairs(parameters))
-        }
-        HttpHead head = new HttpHead(builder.build())
-        return executeHttpRequest(head, responseHandler)
-    }
-
-    protected def executeHttpRequest(HttpRequestBase request, Closure responseHandler) throws Exception{
-        URI uri = request.getURI()
-        log.debug("Executing request: ${request.getMethod()} to URI: ${uri.toString()}")
-
-        if (!client) {
-            this.client = createClient(HttpClientBuilder.create())
-        }
-
-        if(uri.toURL().getProtocol() == "https") {
-            setupSSL(request)
-        }
-
-        def response
-        def returnValue
-        Throwable error
-        try {
-            HttpContext localContext = new BasicHttpContext()
-            response = this.client.execute(request, localContext)
-            returnValue = responseHandler(response)
-
-        } catch (Exception e) {
-            error = e
-            log.error(e, e)
-        } finally {
-            try {
-                if (response) {
-                    cleanupResponse(response)
-                }
-                cleanupRequest(request)
-            } catch (Exception ex) {
-                log.debug(ex, ex)
-            }
-        }
-
-        if (error) {
-            throw error
-        }
-        return returnValue
-    }
-
-    protected static HttpEntity createMultipartEntity(Map<String, Object> params) {
-        MultipartEntityBuilder builder = MultipartEntityBuilder.create()
-        params?.each { k, v ->
-            ContentBody bodyPart
-            if (v instanceof File) {
-                bodyPart = new FileBody(v, Files.probeContentType(v.toPath()), "UTF-8")
-            } else {
-                bodyPart = new StringBody(v.toString())
-            }
-            builder.addPart(k, bodyPart)
-        }
-        return builder.build()
-
-    }
-
-    protected static List<NameValuePair> createBasicNameValuePairs(Map params) {
-        def list = []
-        params?.each{k, v ->
-            list.add(new BasicNameValuePair(k.toString(), v.toString()))
-        }
-        return list
-    }
 
 }
--- src/test/groovy/com/spidasoftware/schema/client/GenericHttpClientSpec.groovy
+++ src/test/groovy/com/spidasoftware/schema/client/GenericHttpClientSpec.groovy
@@ -1,13 +1,18 @@
 package com.spidasoftware.schema.client
 
 import groovy.util.logging.Log4j
+import org.apache.http.HttpEntity
+import org.apache.http.HttpEntityEnclosingRequest
+import org.apache.http.HttpResponse
 import org.apache.http.client.HttpClient
 import org.apache.http.client.methods.CloseableHttpResponse
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.client.methods.HttpUriRequest
+import org.apache.http.entity.mime.MultipartFormEntity
 import org.apache.http.impl.client.CloseableHttpClient
 import org.apache.http.protocol.HttpContext
 import spock.lang.*
+
 import static spock.lang.MockingApi.*
 
 
@@ -21,39 +26,95 @@ import static spock.lang.MockingApi.*
 @Log4j
 class GenericHttpClientSpec extends Specification {
     def client = new GenericHttpClient()
+	URI uri = new URI("http://www.website.com/api")
 
     def setup(){
-        client.createClient = {builder ->
-            def mockResponse = Mock(CloseableHttpResponse)
-            def apacheClient = Mock(CloseableHttpClient)
-            apacheClient.execute(_ as HttpUriRequest, _ as HttpContext) >> mockResponse
-            apacheClient
-        }
-
-        // we're not dealing with real responses, so no need to close streams
-        client.cleanupResponse = {response ->
-            true
-        }
+		client.client = Mock(CloseableHttpClient)
     }
 
-    void "get should call the correct methods and closures"() {
-        setup: "mock the closures"
-        URI uri = new URI("https://www.website.com/api")
-        client.cleanupResponse = {response->
-            assert response
-        }
-        client.setupSSL = {
-           assert it.getMethod() == "GET"
-        }
+	def cleanup(){
+		client.cleanupClient()
+	}
 
-        when: "Execute the request"
-        def response = client.get(uri, [:]){
-            "success"
-        }
+	void "headers should get added to the request"() {
+		setup: ""
+		def headers = ["Content-Type":"text/html", "Accept":"application/json", "Content-Length": "768"]
 
-        then: "the response indicates success"
-        response == "success"
+		when: "create the request"
+		def request = client.createRequest("DELETE", uri, null, headers)
+
+		then: "the request headers should all be present"
+		headers.every{k, v->
+			request.getFirstHeader(k).value == v
+		}
+	}
 
 
-    }
+	void "making a request should call the correct setup/cleanup closures"() {
+		setup: "mock client and response and use an https uri"
+		def httpsUri = new URI("https://www.website.com/api")
+		def mockClient = Mock(CloseableHttpClient)
+		def mockResponse = Mock(CloseableHttpResponse)
+		mockClient.execute(_ as HttpUriRequest, _ as HttpContext) >> mockResponse
+		client.cleanupRequest = Mock(Closure)
+		client.cleanupResponse = Mock(Closure)
+		client.client = mockClient
+
+		when: "execute a request"
+		def result = client.executeRequest("GET", uri, null, null){ "success" }
+
+		then: "the correct closures are called"
+		1*client.cleanupResponse.call(mockResponse)
+		1*client.cleanupRequest.call(_ as HttpUriRequest)
+
+
+	}
+
+	void "POST and PUT requests with files should have the files added to a multipart entity"() {
+		setup: "a dummy file and uri"
+		File f = File.createTempFile("httpClientTest", ".txt")
+		f.deleteOnExit()
+		def testText = "lskdjfoafiewflksdfjsadvoadivjalksajds"
+		f << testText
+
+		when: "create a request with the file as a parameter"
+		def request = client.createRequest("POST", uri, ["testFile": f], null)
+		GenericHttpClient.logRequest(request)
+
+		then: "the request should be of the correct type and contian the file"
+		request instanceof HttpEntityEnclosingRequest
+		def baos = new ByteArrayOutputStream()
+		request.getEntity().writeTo(baos)
+		def text = baos.toString()
+		text.contains(testText)
+		text.contains("Content-Disposition: form-data; name=\"testFile\"")
+		println "Entity text= " + text
+
+	}
+
+	void "requests parameters should be added to the uri for GET, HEAD, and DELETE requests"() {
+
+		when: "create the request"
+		def request = client.createRequest(currentMethod, uri, currentParams, null)
+
+		then: "the uri should match the expected"
+		def uriString = request.getURI().toString()
+		uriString == expectedURI
+
+		where:
+		currentParams << [ ["testKey":"testValue", "limit":5], [apiToken:"57f"], ["w":1, "g":5, "t":true] ]
+		expectedURI << [ "http://www.website.com/api?testKey=testValue&limit=5", "http://www.website.com/api?apiToken=57f", "http://www.website.com/api?w=1&g=5&t=true" ]
+		currentMethod << ["GET", "DELETE", "HEAD"]
+	}
+
+	void "created requests should be of the correct type"() {
+		when: "create the request"
+		def request = client.createRequest(currentMethod, uri, null, null)
+
+		then: "the created request should have the correct method"
+		request.getMethod() == currentMethod
+
+		where:
+		currentMethod << ["GET", "PUT", "POST", "DELETE", "HEAD", "TRACE"]
+	}
 }
