1813746a4cafc7b16bd4f034ffa8d0677ff69de6
pfried <philip.fried@ijus.net>
2014-01-18 08:57:16 -0500

    backup commit

--- src/test/groovy/com/spidasoftware/schema/client/rest/RestAPIResourceSpec.groovy
+++ src/test/groovy/com/spidasoftware/schema/client/rest/RestAPIResourceSpec.groovy
@@ -18,19 +18,39 @@ class RestAPIResourceSpec extends Specification {
 
 
 	void "setting resource properties should work fluently"() {
-		when:
+		when: "create a new api resource and set it's path"
 		resource.path = "/newPath"
 
-		then:
-		resource.path == "/newPath"
+		then: "the new path should be the correct value"
+		resource.settings.path == "/newPath"
 
-		when:
+		when: "add a closure"
 		resource.doWithResponse = {response->
 			"success"
 		}
 
-		then:
+		then: "the closure should be added to the settings"
 		resource.settings.doWithResponse(null) == "success"
+	}
+
+	void "Rest crud methods should call the correct api methods"() {
+		setup: "get a reference to the resource's settings"
+		def config = resource.settings
+		def saveParams = [name:"value"]
+		def id = "12345"
+
+		when: "call save"
+		def saveResult = resource.save(saveParams)
+
+		then: "the api's save method should be called"
+		1*api.save(config, saveParams)
+
+		when: "call find"
+		def findResult = resource.find(id)
+
+		then: "the api's find method should be called"
+		1*api.find(config, id)
+
 	}
 
 }
