9b61ed9550108319b3ead73dfba3aeced3918a77
Eric Smith <eric.smith@spidasoftware.com>
2014-05-28 12:11:21 -0400

    Fixing changeset for userDefinedValues

--- src/main/groovy/com/spidasoftware/schema/changesets/UserDefinedValueChangeSet.groovy
+++ src/main/groovy/com/spidasoftware/schema/changesets/UserDefinedValueChangeSet.groovy
@@ -13,18 +13,22 @@ import net.sf.json.JSONObject
 class UserDefinedValueChangeSet implements ChangeSet {
 
 	String schemaVersion = "0.8"
-	String schemaPath = "/v1/schema/spidacalc/calc/location.schema"
+	String schemaPath = "/v1/schema/spidacalc/calc/project.schema"
 
 	@Override
-	void convert(JSON location) {
-		if (location.userDefinedValues && location.userDefinedValues instanceof JSONArray) {
-			JSONObject newJsonObject = new JSONObject()
+	void convert(JSON project) {
+		project.leads?.each { lead ->
+			lead.locations?.each { location ->
+				if (location.userDefinedValues && location.userDefinedValues instanceof JSONArray) {
+					JSONObject newJsonObject = new JSONObject()
 
-			location.userDefinedValues.each {JSONObject keyValuePair ->
-				newJsonObject.put(keyValuePair.get("key"), keyValuePair.get("value"))
+					location.userDefinedValues.each {JSONObject keyValuePair ->
+						newJsonObject.put(keyValuePair.get("key"), keyValuePair.get("value"))
+					}
+
+					location.put("userDefinedValues", newJsonObject)
+				}
 			}
-
-			location.put("userDefinedValues", newJsonObject)
 		}
 	}
 }
