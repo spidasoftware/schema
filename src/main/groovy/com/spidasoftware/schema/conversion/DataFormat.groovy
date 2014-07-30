0b940e357848d47ca6d468c586723772340adeaa
pfried <philip.fried@ijus.net>
2014-07-30 08:13:21 -0400

    Added DataFormat class

---
+++ src/main/groovy/com/spidasoftware/schema/conversion/DataFormat.groovy
@@ -0,0 +1,14 @@
+package com.spidasoftware.schema.conversion;
+
+import groovy.util.logging.Log4j
+import groovy.transform.CompileStatic
+
+/**
+ * Created by pfried on 7/30/14.
+ */
+
+@Log4j
+@CompileStatic
+public class DataFormat {
+
+}
