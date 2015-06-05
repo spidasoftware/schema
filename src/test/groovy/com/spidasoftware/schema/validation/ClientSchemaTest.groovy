0c48557c5a3003544e5ca29720f27b2dec5c4149
Timothy Overly <timothy.overly@spidasoftware.com>
2015-06-05 10:41:48 -0400

    Fix the client items that didn't match the schema

--- src/test/groovy/com/spidasoftware/schema/validation/ClientSchemaTest.groovy
+++ src/test/groovy/com/spidasoftware/schema/validation/ClientSchemaTest.groovy
@@ -6,22 +6,52 @@ import com.github.fge.jsonschema.main.JsonSchemaFactory
 import com.github.fge.jsonschema.uri.*
 import org.apache.log4j.Logger;
 
-class ClientSchemaTest extends GroovyTestCase { 
+class ClientSchemaTest extends GroovyTestCase {
 
 
 	def log = Logger.getLogger(this.class)
 	def report
 
+	LoadingConfiguration cfg
+	JsonSchemaFactory factory
+
 	void setUp() {
+		 cfg = LoadingConfiguration.newBuilder().setNamespace(new File("resources").toURI().toString()).freeze();
+		 factory = JsonSchemaFactory.newBuilder().setLoadingConfiguration(cfg).freeze();
 	}
 
+
+	private test(String name){
+		def schema = factory.getJsonSchema("v1/schema/spidacalc/client/${name}.schema")
+		report = schema.validate(JsonLoader.fromString(new File("resources/v1/examples/spidacalc/client/client_${name}_example.json").text))
+		report.each{ log.info "validation report "+it.toString() }
+		assertTrue "the instance should be valid against a ${name} schema", report.isSuccess()
+	}
 
 	void testClientPoleObject(){
- 		final LoadingConfiguration cfg = LoadingConfiguration.newBuilder().setNamespace(new File("resources").toURI().toString()).freeze();
-    	final JsonSchemaFactory factory = JsonSchemaFactory.newBuilder().setLoadingConfiguration(cfg).freeze();
-		def schema = factory.getJsonSchema("v1/schema/spidacalc/client/pole.schema")
-		report = schema.validate(JsonLoader.fromString(new File("resources/v1/examples/spidacalc/client/client_pole_example.json").text))
-		report.each{ log.info "validation report "+it.toString() }
-		assertTrue "the instance should be valid against a schema", report.isSuccess()		
+		test("pole")
 	}
+
+	void testClientWireObject(){
+		test("wire")
+	}
+
+	void testClientInsulatorObject(){
+		test("insulator")
+	}
+
+	void testClientAnchorObject(){
+		test("anchor")
+	}
+
+	void testClientEquipmentObject(){
+		test("equipment")
+	}
+
+	void testClientCrossArmObject(){
+		test("crossarm")
+	}
+
+
+
 }
