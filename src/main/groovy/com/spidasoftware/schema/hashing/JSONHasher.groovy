1f130290c953cc7639aebd2e18c038a1aad698c0
Timothy Overly <timothy.overly@spidasoftware.com>
2016-01-29 16:32:27 -0500

    Added a few more methods to the hashing

--- src/main/groovy/com/spidasoftware/schema/hashing/JSONHasher.groovy
+++ src/main/groovy/com/spidasoftware/schema/hashing/JSONHasher.groovy
@@ -12,80 +12,85 @@ class JSONHasher {
 
 	def static jsonSlurper = new JsonSlurper()
 
-	static String hash(String jsonString){
-		Map jsonObject = jsonSlurper.parseText(jsonString)
-		jsonObject.remove('id')
-		jsonObject.remove('version')
-		jsonObject.remove('_id')
-		def mapString = JsonOutput.toJson(sortJsonMap(jsonObject))
+	private static String hashJsonString(String jsonString) {
+		def jsonObject = jsonSlurper.parseText(jsonString)
+		def mapString
+		if (jsonObject instanceof List) {
+			mapString = JsonOutput.toJson(sortAny(jsonObject))
+		} else {
+			jsonObject.remove('id')
+			jsonObject.remove('version')
+			jsonObject.remove('_id')
+			mapString = JsonOutput.toJson(sortJsonMap(jsonObject))
+		}
 		return DigestUtils.md5Hex(mapString)
 	}
 
-	static String hash(Map jsonObject){
-		//Do this to ensure and copy json
-		return hash(JsonOutput.toJson(jsonObject))
+	static String hash(List jsonList) {
+		return hashJsonString(JsonOutput.toJson(jsonList))
+	}
+
+	static String hash(Map jsonObject) {
+		return hashJsonString(JsonOutput.toJson(jsonObject))
 	}
 
 	//Partially recursive (uses sortAny() which also can call this function) sort of a map.
-	static Map sortJsonMap(Map jsonMap){
+	static Map sortJsonMap(Map jsonMap) {
 
-		try {
-			def id
-			def version
-			if (jsonMap.id != null){
-				id = jsonMap.remove("id")
-			}
-			if (jsonMap.version != null){
-				version = jsonMap.remove("version")
-			}
-
-			Map sortedMap = [:]
-
-			if(id){
-				sortedMap.put("id", id)
-			}
-
-			if(version){
-				sortedMap.put("version", version)
-			}
-
-			sortedMap.putAll((Map) sortAny(jsonMap))
-
-			return sortedMap
-		} catch (NullPointerException e){
-			log.error("Cannot pass null map", e)
+		def id
+		def version
+		if (jsonMap.id != null) {
+			id = jsonMap.remove("id")
 		}
+		if (jsonMap.version != null) {
+			version = jsonMap.remove("version")
+		}
+
+		Map sortedMap = [:]
+
+		if (id) {
+			sortedMap.put("id", id)
+		}
+
+		if (version) {
+			sortedMap.put("version", version)
+		}
+
+		sortedMap.putAll((Map) sortAny(jsonMap))
+
+		return sortedMap
+
 	}
 
 	//Recursively sorts json data.  Partner with sortJsonMap
-	public static Object sortAny(def data){
+	public static Object sortAny(def data) {
 
-		if (data instanceof List){
-			log.trace("sorting list:"+ data)
+		if (data instanceof List) {
+			log.trace("sorting list:" + data)
 			def childrenSorted = data.collect {
 				return sortAny(it)
 			}
-			childrenSorted = childrenSorted.sort {it.toString()}
-			log.trace("sorted list:"+ childrenSorted)
+			childrenSorted = childrenSorted.sort { it.toString() }
+			log.trace("sorted list:" + childrenSorted)
 			return childrenSorted
-		} else if (data instanceof Map){
-			log.trace("sorting map:"+ data)
+		} else if (data instanceof Map) {
+			log.trace("sorting map:" + data)
 			def key, value
 			def sortedKeys = data.keySet().sort()
 			def sortedMap = [:]
-			for( int i = 0; i < data.size(); i++){
+			for (int i = 0; i < data.size(); i++) {
 				key = sortedKeys[i]
 				value = data[key]
 
 				//Checks if the type of the value is BigDecimal, and then cuts it
 				//to a floating point to avoid a MongoDB bug with BDs.
-				if ( value instanceof BigDecimal ){
+				if (value instanceof BigDecimal) {
 					log.trace("Found Big Decimal, converting to a double")
 					value = (double) value.doubleValue()
 				}
 				sortedMap.put(key.toString(), sortAny(value))
 			}
-			log.trace("sorted map:"+ sortedMap)
+			log.trace("sorted map:" + sortedMap)
 			return sortedMap
 
 
