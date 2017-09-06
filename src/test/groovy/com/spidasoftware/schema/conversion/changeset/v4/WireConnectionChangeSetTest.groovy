7a2929cafb25eabdae696c4f0c6d338c9c891a6a
Jason Garrett <jason.garrett@spidasoftware.com>
2017-09-06 13:44:01 -0400

    revert wire.connectedWire [finishes #150847222]

--- src/test/groovy/com/spidasoftware/schema/conversion/changeset/v4/WireConnectionChangeSetTest.groovy
+++ src/test/groovy/com/spidasoftware/schema/conversion/changeset/v4/WireConnectionChangeSetTest.groovy
@@ -4,29 +4,32 @@ import com.spidasoftware.schema.conversion.changeset.ChangeSet
 import groovy.json.JsonSlurper
 import spock.lang.Specification
 
-class WireConnectionIdChangeSetTest extends Specification {
+class WireConnectionChangeSetTest extends Specification {
 
     def "revert"() {
         setup:
-            def leanStream = WireConnectionIdChangeSet.getResourceAsStream("/conversions/v4/wire-connection-id.json")
+            def leanStream = WireConnectionChangeSet.getResourceAsStream("/conversions/v4/wire-connection.json")
             Map projectJSON = new JsonSlurper().parse(leanStream)
             Map locationJSON = ChangeSet.duplicateAsJson(projectJSON.leads[0].locations[0])
             Map designJSON = ChangeSet.duplicateAsJson(projectJSON.leads[0].locations[0].designs[0])
-            WireConnectionIdChangeSet changeSet = new WireConnectionIdChangeSet()
+            WireConnectionChangeSet changeSet = new WireConnectionChangeSet()
         when: "revertProject"
             changeSet.revertProject(projectJSON)
             def wire = projectJSON.leads.first().locations.first().designs.first().structure.wires.first()
         then:
             wire.connectionId == null
+            wire.connectedWire == null
         when: "revertLocation"
             changeSet.revertLocation(locationJSON)
             wire = locationJSON.designs.first().structure.wires.first()
         then:
             wire.connectionId == null
+            wire.connectedWire == null
         when: "revertDesign"
             changeSet.revertDesign(designJSON)
             wire = designJSON.structure.wires.first()
         then:
             wire.connectionId == null
+            wire.connectedWire == null
     }
 }
