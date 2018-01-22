46904454ee10b19f2d78e94cf4c197489567062f
Andy Strominger <andrew.strominger@spidasoftware.com>
2018-01-22 11:10:45 -0500

    [Finishes #154485061] Remove schema and version from locations and designs when reverting from schema version 4

---
+++ src/main/groovy/com/spidasoftware/schema/conversion/changeset/v4/RemoveSchemaAndVersionChangeSet.groovy
@@ -0,0 +1,44 @@
+package com.spidasoftware.schema.conversion.changeset.v4
+
+import com.spidasoftware.schema.conversion.changeset.ConversionException
+import com.spidasoftware.schema.conversion.changeset.calc.CalcProjectChangeSet
+
+class RemoveSchemaAndVersionChangeSet extends CalcProjectChangeSet {
+
+    @Override
+    void applyToProject(Map projectJSON) throws ConversionException {
+        // Do nothing
+    }
+
+    @Override
+    void revertProject(Map projectJSON) throws ConversionException {
+        forEachLocation(projectJSON, { Map locationJSON ->
+            revertLocation(locationJSON)
+        })
+    }
+
+    @Override
+    void applyToLocation(Map locationJSON) throws ConversionException {
+        // Do nothing
+    }
+
+    @Override
+    void revertLocation(Map locationJSON) throws ConversionException {
+        locationJSON.remove("schema")
+        locationJSON.remove("version")
+        locationJSON.get("designs")?.each { Map designJSON ->
+            revertDesign(designJSON)
+        }
+    }
+
+    @Override
+    void applyToDesign(Map designJSON) throws ConversionException {
+        // Do nothing
+    }
+
+    @Override
+    void revertDesign(Map designJSON) throws ConversionException {
+        designJSON.remove("schema")
+        designJSON.remove("version")
+    }
+}
