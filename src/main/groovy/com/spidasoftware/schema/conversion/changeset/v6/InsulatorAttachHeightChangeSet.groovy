d91b42d2c9d8a9d762dfc99d32ca3729a7493703
Luanhnguyen07 <Luan.nguyen@spidasoftware.com>
2018-12-28 16:17:18 -0500

    changeset for insulator

---
+++ src/main/groovy/com/spidasoftware/schema/conversion/changeset/v6/InsulatorAttachHeightChangeSet.groovy
@@ -0,0 +1,45 @@
+/*
+ * ©2009-2018 SPIDAWEB LLC
+ */
+package com.spidasoftware.schema.conversion.changeset.v6
+
+import com.spidasoftware.schema.conversion.changeset.ConversionException
+import com.spidasoftware.schema.conversion.changeset.calc.AbstractCalcDesignChangeset
+import groovy.util.logging.Log4j
+
+/**
+ * In the schema v6 release we started exporting detailed results alongside summary results.  We also added the resultId
+ * reference to detailed results.  When down converteing if we have detailed results in the design analysis we remove them,
+ * then we still have the summary results because we always export summary results.
+ * We also remove the analysis if it references detailed results as it isn't valid in previous versions.
+ */
+@Log4j
+class InsulatorAttachHeightChangeSet extends AbstractCalcDesignChangeset {
+
+	@Override
+	void applyToDesign(Map design) throws ConversionException {
+		def insulatorWithCrossArmParentId = []
+		design.get("structure")?.get("crossArms")?.each { crossArm ->
+			if (crossArm.containsKey("insulators")) {
+				insulatorWithCrossArmParentId.addAll(crossArm.get("insulators"))
+			}
+		}
+
+		design.get("structure")?.get("insulators")?.each { insulator ->
+			if (!insulatorWithCrossArmParentId.contains(insulator.get("id"))) {
+				insulator.put("attachmentHeight", insulator.get("offset"))
+				insulator.remove("offset")
+			}
+		}
+	}
+
+	@Override
+	void revertDesign(Map design) throws ConversionException {
+		design.get("structure")?.get("insulators")?.each { insulator ->
+			if (insulator.containsKey("attachmentHeight")) {
+				insulator.remove("attachmentHeight")
+				insulator.put("offset", insulator.get("attachmentHeight"))
+			}
+		}
+	}
+}
---
+++ src/test/groovy/com/spidasoftware/schema/conversion/changeset/v6/InsulatorAttachHeightChangeSetTest.groovy
@@ -0,0 +1,33 @@
+package com.spidasoftware.schema.conversion.changeset.v6
+
+import com.spidasoftware.schema.validation.Validator
+import groovy.json.JsonSlurper
+import spock.lang.Specification
+
+class InsulatorAttachHeightChangeSetTest extends Specification {
+
+	void "test revert file with wire tension"() {
+		def leanStream = RevertBundleChangesetTest.getResourceAsStream("/conversions/v6/wire-tension.json".toString())
+		Map projectJSON = new JsonSlurper().parse(leanStream)
+		leanStream.close()
+
+
+		def designs = projectJSON.leads[0].locations*.designs.flatten()
+		expect:
+		designs[0].analysis[0].results.size() == 12
+		designs[0].analysis[0].results[11].analysisType == "TENSION"
+
+		when:
+		def changeset = new RemoveTensionResultsChangeset()
+		changeset.revertProject(projectJSON)
+		def firstResults = projectJSON.leads[0].locations[0].designs[0].analysis[0].results
+		then:
+		!designs*.analysis.flatten().any { analysis ->
+			analysis.results?.any { result ->
+				result.analysisType == "TENSION"
+			}
+		}
+
+		firstResults.size() == 11
+	}
+}
