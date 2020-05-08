5d65515e5eb7a0009f011961570f682c1ba7edc7
Luanhnguyen07 <Luan.nguyen@spidasoftware.com>
2020-05-08 11:56:30 -0400

    downconvert to inclination and upgrade to relative elevation

--- src/test/groovy/com/spidasoftware/schema/conversion/changeset/v7/RelativeElevationChangeSetTest.groovy
+++ src/test/groovy/com/spidasoftware/schema/conversion/changeset/v7/RelativeElevationChangeSetTest.groovy
@@ -8,13 +8,43 @@ import spock.lang.Specification
 
 class RelativeElevationChangeSetTest extends Specification {
 
+	def "ApplyToDesign"() {
+		def stream = RelativeElevationChangeSet.getResourceAsStream("/conversions/v7/RelativeElevationApplyToDesign.json".toString())
+		Map json = new JsonSlurper().parse(stream)
+		stream.close()
+		when:
+			new RelativeElevationChangeSet().applyToDesign(json)
+		then:
+			//from metre and radian
+			json.structure.wireEndPoints[0].relativeElevation.value == 7.9391946829320545
+			json.structure.wireEndPoints[0].relativeElevation.unit == "METRE"
+			//from foot and degree angle
+			json.structure.wireEndPoints[1].relativeElevation.value == 26.047226650039548
+			json.structure.wireEndPoints[1].relativeElevation.unit == "FOOT"
+			//removed inclination
+			json.structure.wireEndPoints[0].inclination == null
+			json.structure.wireEndPoints[1].inclination == null
+	}
+
 	def "RevertDesign"() {
-		def stream = RelativeElevationChangeSet.getResourceAsStream("/conversions/v7/relativeElevation.json".toString())
+		def stream = RelativeElevationChangeSet.getResourceAsStream("/conversions/v7/RelativeElevationRevertDesign.json".toString())
 		Map json = new JsonSlurper().parse(stream)
 		stream.close()
 		when:
 			new RelativeElevationChangeSet().revertDesign(json)
 		then:
-			json.structure.wireEndPoints[0].relativeElevation == null
+		//from metre
+		json.structure.wireEndPoints[0].inclination.value == 0.08693354666361731
+		json.structure.wireEndPoints[0].inclination.unit == "RADIAN"
+		//from foot
+		json.structure.wireEndPoints[1].inclination.value == 0.17453292519943295
+		json.structure.wireEndPoints[1].inclination.unit == "RADIAN"
+		//mix and match foot and metre
+		json.structure.wireEndPoints[2].inclination.value == 0.07947558659675234
+		json.structure.wireEndPoints[2].inclination.unit == "RADIAN"
+		//removed inclination
+		json.structure.wireEndPoints[0].relativeElevation == null
+		json.structure.wireEndPoints[1].relativeElevation == null
+		json.structure.wireEndPoints[2].relativeElevation == null
 	}
 }
