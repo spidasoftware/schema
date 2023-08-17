d19d15a1bf8c323fc22772dd544ff0e4c8e1cb74
Dillon Todd <110418379+spidaDillon@users.noreply.github.com>
2023-08-17 09:05:41 -0500

    add new nesc extreme wind load case [finishes #185711253]

--- src/main/groovy/com/spidasoftware/schema/conversion/changeset/v10/LoadCaseChangeSet.groovy
+++ src/main/groovy/com/spidasoftware/schema/conversion/changeset/v10/LoadCaseChangeSet.groovy
@@ -8,62 +8,36 @@ import com.spidasoftware.schema.conversion.changeset.client.AbstractClientDataCh
 import groovy.transform.CompileStatic
 
 /**
- * The 2015 CSA load case name was changed to say '2015-2020'
+ * New CSA Max wind load case was added after Calc v8.0
  *
- * All 2012 NESC load case names were changed to say '2012-2017'
- *
- * When up-converting, we will change '2015' to '2015-2020' for CSA load cases and '2012' to '2012-2017' for NESC
- * When down-converting, we will change '2015-2020' to '2015' for CSA and '2012-2017' to '2012' for NESC
+ * When down converting from schema v10, we want to remove the entire load case
  */
 @CompileStatic
 class LoadCaseChangeSet extends AbstractClientDataChangeSet {
-
     @Override
     boolean applyToClientData(Map clientDataJSON) throws ConversionException {
-        boolean anyChanged = false
-
-        clientDataJSON.analysisCases?.each { Map analysisCase ->
-            anyChanged |= applyName(analysisCase)
-        }
-        return anyChanged
+        return false
     }
 
     @Override
     boolean revertClientData(Map clientDataJSON) throws ConversionException {
         boolean anyChanged = false
 
-        clientDataJSON.analysisCases?.each { Map analysisCase ->
-            anyChanged |= revertName(analysisCase)
+        if (clientDataJSON.containsKey("analysisCases")) {
+            List<Map> analysisCases = clientDataJSON.analysisCases as List<Map>
+            anyChanged = analysisCases.removeAll { it.type == "CSA 2020 Maximum Wind" || it.type == "NESC Extreme Wind 2023" }
         }
+
         return anyChanged
-    }
-
-    @Override
-    void applyToProject(Map projectJSON) throws ConversionException {
-        super.applyToProject(projectJSON)
-
-        projectJSON.defaultLoadCases?.each { Map loadCase ->
-            applyName(loadCase)
-        }
     }
 
     @Override
     void revertProject(Map projectJSON) throws ConversionException {
         super.revertProject(projectJSON)
 
-        projectJSON.defaultLoadCases?.each { Map loadCase ->
-            revertName(loadCase)
-        }
-    }
-
-    @Override
-    void applyToDesign(Map designJSON) throws ConversionException {
-        super.applyToDesign(designJSON)
-
-        designJSON.analysis?.each { Map analysis ->
-            if (analysis.containsKey("analysisCaseDetails")) {
-                applyName(analysis.analysisCaseDetails as Map)
-            }
+        if (projectJSON.containsKey("defaultLoadCases")) {
+            List<Map> defaultLoadCases = projectJSON.defaultLoadCases as List<Map>
+            defaultLoadCases.removeAll { it.type == "CSA 2020 Maximum Wind" || it.type == "NESC Extreme Wind 2023" }
         }
     }
 
@@ -71,73 +45,20 @@ class LoadCaseChangeSet extends AbstractClientDataChangeSet {
     void revertDesign(Map designJSON) throws ConversionException {
         super.revertDesign(designJSON)
 
-        designJSON.analysis?.each { Map analysis ->
-            if (analysis.containsKey("analysisCaseDetails")) {
-                revertName(analysis.analysisCaseDetails as Map)
-            }
+        if (designJSON.containsKey("analysis")) {
+            List<Map> analysisList = designJSON.analysis as List<Map>
+            analysisList.removeAll { (it.analysisCaseDetails as Map).type == "CSA 2020 Maximum Wind" || (it.analysisCaseDetails as Map).type == "NESC Extreme Wind 2023" }
         }
-    }
-
-    @Override
-    boolean applyToResults(Map resultsJSON) throws ConversionException {
-        boolean anyChanged = super.applyToResults(resultsJSON)
-
-        resultsJSON.results?.each { Map result ->
-            if (result.containsKey("analysisCaseDetails")) {
-                anyChanged |= applyName(result.analysisCaseDetails as Map)
-            }
-
-        }
-        return anyChanged
     }
 
     @Override
     boolean revertResults(Map resultsJSON) throws ConversionException {
         boolean anyChanged = super.revertResults(resultsJSON)
 
-        resultsJSON.results?.each { Map result ->
-            if (result.containsKey("analysisCaseDetails")) {
-                anyChanged |= revertName(result.analysisCaseDetails as Map)
-            }
+        if (resultsJSON.containsKey("results")) {
+            List<Map> resultsList = resultsJSON.results as List<Map>
+            anyChanged = resultsList.removeAll { (it.analysisCaseDetails as Map).type == "CSA 2020 Maximum Wind" || (it.analysisCaseDetails as Map).type == "NESC Extreme Wind 2023" }
         }
         return anyChanged
-    }
-
-    boolean applyName(Map loadCase) {
-        switch (loadCase.type) {
-            case "CSA 2015":
-                loadCase.type = "CSA 2015-2020"
-                return true
-            case "NESC 2012":
-                loadCase.type = "NESC 2012-2017"
-                return true
-            case "NESC Extreme Wind 2012":
-                loadCase.type = "NESC Extreme Wind 2012-2017"
-                return true
-            case "NESC Extreme Ice 2012":
-                loadCase.type = "NESC Extreme Ice 2012-2017"
-                return true
-            default:
-                return false
-        }
-    }
-
-    boolean revertName(Map loadCase) {
-        switch (loadCase.type) {
-            case "CSA 2015-2020":
-                loadCase.type = "CSA 2015"
-                return true
-            case "NESC 2012-2017":
-                loadCase.type = "NESC 2012"
-                return true
-            case "NESC Extreme Wind 2012-2017":
-                loadCase.type = "NESC Extreme Wind 2012"
-                return true
-            case "NESC Extreme Ice 2012-2017":
-                loadCase.type = "NESC Extreme Ice 2012"
-                return true
-            default:
-                return false
-        }
     }
 }
---
+++ src/test/groovy/com/spidasoftware/schema/conversion/changeset/v10/LoadCaseNameChangeSetTest.groovy
@@ -0,0 +1,114 @@
+/*
+ * ©2009-2023 SPIDAWEB LLC
+ */
+package com.spidasoftware.schema.conversion.changeset.v10
+
+import groovy.json.JsonSlurper
+import spock.lang.Specification
+
+class LoadCaseNameChangeSetTest extends Specification {
+
+    static LoadCaseNameChangeSet changeSet
+
+    def setupSpec() {
+        changeSet = new LoadCaseNameChangeSet()
+    }
+
+    def "apply/revert client data"() {
+        setup:
+            def stream = LoadCaseNameChangeSetTest.getResourceAsStream("/conversions/v10/LoadCaseIceDensity-client.json".toString())
+            Map json = new JsonSlurper().parse(stream) as Map
+            stream.close()
+            boolean anyChanged
+        expect:
+            json.analysisCases[9].type == "NESC 2012"
+            json.analysisCases[15].type == "NESC Extreme Wind 2012"
+            json.analysisCases[16].type == "NESC Extreme Ice 2012"
+            json.analysisCases[17].type == "CSA 2015"
+        when: "apply to client data"
+            anyChanged = changeSet.applyToClientData(json)
+        then: "types get updated"
+            anyChanged
+            json.analysisCases[9].type == "NESC 2012-2017"
+            json.analysisCases[15].type == "NESC Extreme Wind 2012-2017"
+            json.analysisCases[16].type == "NESC Extreme Ice 2012-2017"
+            json.analysisCases[17].type == "CSA 2015-2020"
+        when: "revert client data"
+            anyChanged = changeSet.revertClientData(json)
+        then: "types get updated"
+            anyChanged
+            json.analysisCases[9].type == "NESC 2012"
+            json.analysisCases[15].type == "NESC Extreme Wind 2012"
+            json.analysisCases[16].type == "NESC Extreme Ice 2012"
+            json.analysisCases[17].type == "CSA 2015"
+    }
+
+    def "apply/revert project json"() {
+        setup:
+            def stream = LoadCaseNameChangeSetTest.getResourceAsStream("/conversions/v10/LoadCaseIceDensity-project.json".toString())
+            Map json = new JsonSlurper().parse(stream) as Map
+            stream.close()
+        expect:
+            json.defaultLoadCases[6].type == "NESC 2012"
+            json.defaultLoadCases[12].type == "NESC Extreme Wind 2012"
+            json.defaultLoadCases[13].type == "NESC Extreme Ice 2012"
+            json.defaultLoadCases[14].type == "CSA 2015"
+            json.leads[0].locations[0].designs[0].analysis[6].analysisCaseDetails.type == "NESC 2012"
+            json.leads[0].locations[0].designs[0].analysis[11].analysisCaseDetails.type == "CSA 2015"
+            json.leads[0].locations[0].designs[0].analysis[12].analysisCaseDetails.type == "NESC Extreme Wind 2012"
+            json.leads[0].locations[0].designs[0].analysis[13].analysisCaseDetails.type == "NESC Extreme Ice 2012"
+
+        when:
+            changeSet.applyToProject(json)
+        then:
+            json.defaultLoadCases[6].type == "NESC 2012-2017"
+            json.defaultLoadCases[12].type == "NESC Extreme Wind 2012-2017"
+            json.defaultLoadCases[13].type == "NESC Extreme Ice 2012-2017"
+            json.defaultLoadCases[14].type == "CSA 2015-2020"
+            json.leads[0].locations[0].designs[0].analysis[6].analysisCaseDetails.type == "NESC 2012-2017"
+            json.leads[0].locations[0].designs[0].analysis[11].analysisCaseDetails.type == "CSA 2015-2020"
+            json.leads[0].locations[0].designs[0].analysis[12].analysisCaseDetails.type == "NESC Extreme Wind 2012-2017"
+            json.leads[0].locations[0].designs[0].analysis[13].analysisCaseDetails.type == "NESC Extreme Ice 2012-2017"
+        when:
+            changeSet.revertProject(json)
+        then:
+            json.defaultLoadCases[6].type == "NESC 2012"
+            json.defaultLoadCases[12].type == "NESC Extreme Wind 2012"
+            json.defaultLoadCases[13].type == "NESC Extreme Ice 2012"
+            json.defaultLoadCases[14].type == "CSA 2015"
+            json.leads[0].locations[0].designs[0].analysis[6].analysisCaseDetails.type == "NESC 2012"
+            json.leads[0].locations[0].designs[0].analysis[11].analysisCaseDetails.type == "CSA 2015"
+            json.leads[0].locations[0].designs[0].analysis[12].analysisCaseDetails.type == "NESC Extreme Wind 2012"
+            json.leads[0].locations[0].designs[0].analysis[13].analysisCaseDetails.type == "NESC Extreme Ice 2012"
+    }
+
+    def "apply/revert results json"() {
+        setup:
+            def stream = LoadCaseNameChangeSetTest.getResourceAsStream("/conversions/v10/LoadCaseIceDensity-results.json".toString())
+            Map json = new JsonSlurper().parse(stream) as Map
+            stream.close()
+            boolean anyChanged
+        expect:
+            json.results[1].analysisCaseDetails.type == "CSA 2015"
+            json.results[2].analysisCaseDetails.type == "NESC Extreme Wind 2012"
+            json.results[3].analysisCaseDetails.type == "NESC Extreme Ice 2012"
+            json.results[9].analysisCaseDetails.type == "NESC 2012"
+        when:
+            anyChanged = changeSet.applyToResults(json)
+        then:
+            anyChanged
+            json.results[1].analysisCaseDetails.type == "CSA 2015-2020"
+            json.results[2].analysisCaseDetails.type == "NESC Extreme Wind 2012-2017"
+            json.results[3].analysisCaseDetails.type == "NESC Extreme Ice 2012-2017"
+            json.results[9].analysisCaseDetails.type == "NESC 2012-2017"
+        when:
+            anyChanged = changeSet.revertResults(json)
+        then:
+            anyChanged
+            json.results[1].analysisCaseDetails.type == "CSA 2015"
+            json.results[2].analysisCaseDetails.type == "NESC Extreme Wind 2012"
+            json.results[3].analysisCaseDetails.type == "NESC Extreme Ice 2012"
+            json.results[9].analysisCaseDetails.type == "NESC 2012"
+    }
+
+}
