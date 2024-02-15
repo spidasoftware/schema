3e042cd686cd7b368361a77fcaabe24d4610227a
Dillon Todd <110418379+spidaDillon@users.noreply.github.com>
2024-02-15 13:21:48 -0600

    add schema validation test

--- src/test/groovy/com/spidasoftware/schema/conversion/changeset/v11/ClientPoleSettingTypeChangeSetTest.groovy
+++ src/test/groovy/com/spidasoftware/schema/conversion/changeset/v11/ClientPoleSettingTypeChangeSetTest.groovy
@@ -3,6 +3,8 @@
  */
 package com.spidasoftware.schema.conversion.changeset.v11
 
+import com.github.fge.jsonschema.core.report.ProcessingReport
+import com.spidasoftware.schema.validation.Validator
 import groovy.json.JsonSlurper
 import spock.lang.Specification
 
@@ -56,5 +58,48 @@ class ClientPoleSettingTypeChangeSetTest extends Specification {
             !json.poles[1].customSettingDepth
             !json.poles[2].settingType
             !json.poles[2].customSettingDepth
+    }
+
+    def "test schema validation"() {
+        setup:
+            InputStream stream = ClientPoleSettingTypeChangeSetTest.getResourceAsStream("/conversions/v11/ClientPole-client-v11.json")
+            Map json = new JsonSlurper().parse(stream) as Map
+            stream.close()
+            Validator validator = new Validator()
+            ProcessingReport report
+        expect:
+            json.poles[0].settingType == "ANSI"
+            !json.poles[0].customSettingDepth
+            json.poles[1].settingType == "GO95"
+            !json.poles[1].customSettingDepth
+            json.poles[2].settingType == "CUSTOM"
+            json.poles[2].customSettingDepth == [value: 1, unit: "METRE"]
+        when: "validate pole with ANSI setting type and no customSettingDepth"
+            report = validator.validateAndReport("/schema/spidacalc/client/pole.schema", json.poles[0] as Map)
+        then: "valid"
+            report.success
+        when: "validate pole with ANSI setting type and customSettingDepth"
+            json.poles[0].customSettingDepth = [value: 1, unit: "METRE"]
+            report = validator.validateAndReport("/schema/spidacalc/client/pole.schema", json.poles[0] as Map)
+        then: "invalid"
+            !report.success
+        when: "validate pole with GO95 setting type and no customSettingDepth"
+            report = validator.validateAndReport("/schema/spidacalc/client/pole.schema", json.poles[1] as Map)
+        then: "valid"
+            report.success
+        when: "validate pole with GO95 setting type and customSettingDepth"
+            json.poles[1].customSettingDepth = [value: 1, unit: "METRE"]
+            report = validator.validateAndReport("/schema/spidacalc/client/pole.schema", json.poles[1] as Map)
+        then: "invalid"
+            !report.success
+        when: "validate pole with CUSTOM setting type and customSettingDepth"
+            report = validator.validateAndReport("/schema/spidacalc/client/pole.schema", json.poles[2] as Map)
+        then: "valid"
+            report.success
+        when: "validate pole with CUSTOM setting type and no customSettingDepth"
+            json.poles[2].remove("customSettingDepth")
+            report = validator.validateAndReport("/schema/spidacalc/client/pole.schema", json.poles[2] as Map)
+        then: "invalid"
+            !report.success
     }
 }
