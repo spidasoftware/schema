163d3dcf54fd0288c20966416a10fd071d3affdb
Dillon Todd <110418379+spidaDillon@users.noreply.github.com>
2024-04-17 08:57:57 -0500

    add moment at height changeset for client poles

--- src/main/groovy/com/spidasoftware/schema/conversion/changeset/ConverterUtils.groovy
+++ src/main/groovy/com/spidasoftware/schema/conversion/changeset/ConverterUtils.groovy
@@ -7,6 +7,7 @@ import com.spidasoftware.schema.conversion.changeset.calc.*
 import com.spidasoftware.schema.conversion.changeset.client.ClientDataConverter
 import com.spidasoftware.schema.conversion.changeset.v10.*
 import com.spidasoftware.schema.conversion.changeset.v11.ClientPoleSettingTypeChangeSet
+import com.spidasoftware.schema.conversion.changeset.v11.MomentAtHeightChangeSet
 import com.spidasoftware.schema.conversion.changeset.v11.PoleCutTopChangeSet
 import com.spidasoftware.schema.conversion.changeset.v2.*
 import com.spidasoftware.schema.conversion.changeset.v3.*
@@ -77,6 +78,7 @@ class ConverterUtils {
         converter.addChangeSet(10, new TemperatureOverridesChangeSet())
         converter.addChangeSet(11, new ClientPoleSettingTypeChangeSet())
         converter.addChangeSet(11, new PoleCutTopChangeSet())
+        converter.addChangeSet(11, new MomentAtHeightChangeSet())
 
         // add calc changesets above here
         converter.setCurrentVersion(currentVersion)
@@ -96,6 +98,7 @@ class ConverterUtils {
         converter.addChangeSet(10, new DecimalDirectionsChangeset())
         converter.addChangeSet(10, new TemperatureOverridesChangeSet())
         converter.addChangeSet(11, new ClientPoleSettingTypeChangeSet())
+        converter.addChangeSet(11, new MomentAtHeightChangeSet())
         // add client data changesets above here
 
         converter.setCurrentVersion(currentVersion)
