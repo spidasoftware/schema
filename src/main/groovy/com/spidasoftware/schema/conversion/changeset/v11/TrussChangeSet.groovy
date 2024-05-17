70f0adea970a79f8b0e3a3e8bf014bdd087c7eee
Dillon Todd <110418379+spidaDillon@users.noreply.github.com>
2024-05-17 08:02:37 -0500

    update changeset and its tests

--- src/main/groovy/com/spidasoftware/schema/conversion/changeset/v11/TrussChangeSet.groovy
+++ src/main/groovy/com/spidasoftware/schema/conversion/changeset/v11/TrussChangeSet.groovy
@@ -16,6 +16,34 @@ class TrussChangeSet extends AbstractClientDataChangeSet {
 
     @Override
     boolean revertClientData(Map clientDataJSON) throws ConversionException {
-        return clientDataJSON.remove("trusses") != null
+        boolean anyChanged = clientDataJSON.remove("trusses") != null
+
+        clientDataJSON.assemblies?.each { Map assembly ->
+            anyChanged |= revertStructure(assembly.assemblyStructure as Map)
+        }
+        return anyChanged
+    }
+
+    @Override
+    void revertDesign(Map designJSON) throws ConversionException {
+        super.revertDesign(designJSON)
+
+        if (designJSON.structure != null) {
+            revertStructure(designJSON.structure as Map)
+        }
+    }
+
+    @Override
+    boolean revertResults(Map resultsJSON) throws ConversionException {
+        super.revertResults(resultsJSON)
+
+        if (resultsJSON.analyzedStructure != null) {
+            return revertStructure(resultsJSON.analyzedStructure as Map)
+        }
+        return false
+    }
+
+    protected boolean revertStructure(Map structureJSON) {
+        return structureJSON.remove("trusses") != null
     }
 }
