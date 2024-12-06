581162116048c045a0d78cddd344434e40a1d69b
Jason Garrett <jason.garrett@spidasoftware.com>
2024-12-06 17:51:46 -0500

    applying to design also applies to results

--- src/main/groovy/com/spidasoftware/schema/conversion/changeset/v12/PushBraceHeightChangeSet.groovy
+++ src/main/groovy/com/spidasoftware/schema/conversion/changeset/v12/PushBraceHeightChangeSet.groovy
@@ -5,55 +5,57 @@ package com.spidasoftware.schema.conversion.changeset.v12
 
 import com.spidasoftware.schema.conversion.changeset.ConversionException
 import com.spidasoftware.schema.conversion.changeset.calc.AbstractResultsChangeSet
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
 
+@CompileStatic
+@Slf4j
 class PushBraceHeightChangeSet extends AbstractResultsChangeSet {
 
 	@Override
 	void applyToDesign(Map design) throws ConversionException {
-		design.get("structure")?.get("pushBraces")?.each { Map pushBrace ->
-			def measurable = [:]
-			measurable.put("value", 0)
-			measurable.put("unit", "METRE")
-			pushBrace.put("height", measurable)
+		applyToStructure(design.structure as Map)
+		if (design.containsKey("analysisDetails") && ((Map)design.analysisDetails).containsKey("detailedResults")) {
+			Map detailedResultsJSON = ((Map)design.analysisDetails).detailedResults as Map
+			applyToResults(detailedResultsJSON)
 		}
 	}
 
 	@Override
 	void revertDesign(Map design) throws ConversionException {
-		design.get("structure")?.get("pushBraces")?.each { Map pushBrace ->
-			def height = pushBrace.get("height")
-			if (height) {
-				pushBrace.remove("height")
-			}
+		revertStructure(design.structure as Map)
+		if (design.containsKey("analysisDetails") && ((Map)design.analysisDetails).containsKey("detailedResults")) {
+			Map detailedResultsJSON = ((Map)design.analysisDetails).detailedResults as Map
+			revertResults(detailedResultsJSON)
 		}
 	}
 
 	@Override
 	boolean revertResults(Map resultsJSON) throws ConversionException {
-		resultsJSON.get("analyzedStructure")?.get("pushBraces")?.each { Map pushBrace ->
-			pushBrace.remove("height")
-		}
-		return !resultsJSON.analyzedStructure?.get("pushBraces")?.last()?.height
+		return revertStructure(resultsJSON.analyzedStructure as Map)
 	}
 
 	@Override
 	boolean applyToResults(Map resultsJSON) throws ConversionException {
-		applyToStructure(resultsJSON)
-		if (resultsJSON.containsKey("analysisDetails") && ((Map)resultsJSON.analysisDetails).containsKey("detailedResults")) {
-			Map detailedResultsJson = ((Map)resultsJSON.analysisDetails).detailedResults as Map
-			return applyToStructure(detailedResultsJson)
-		}
-		return false
+		return applyToStructure(resultsJSON.analyzedStructure as Map)
 	}
 
 	protected boolean applyToStructure(Map structureJSON) {
-		structureJSON.get("analyzedStructure")?.get("pushBraces")?.each { Map pushBrace ->
+		structureJSON?.get("pushBraces")?.each { Map pushBrace ->
 			def measurable = [:]
 			measurable.put("value", 0)
 			measurable.put("unit", "METRE")
 			pushBrace.put("height", measurable)
 		}
-		List pushBraces = structureJSON.analyzedStructure?.get("pushBraces")
-		return pushBraces ? pushBraces.last()?.height : false
+		return false  // the height was already implied to be zero
+	}
+
+	protected boolean revertStructure(Map structureJSON) {
+		boolean changed = false
+		structureJSON?.get("pushBraces")?.each { Map pushBrace ->
+			pushBrace.remove("height")
+			changed = true
+		}
+		return changed
 	}
 }
