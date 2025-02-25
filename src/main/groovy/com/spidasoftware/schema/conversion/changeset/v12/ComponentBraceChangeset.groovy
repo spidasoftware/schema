c34a86718364d10a3cb03f18e30e789b4135c902
Jason Garrett <jason.garrett@spidasoftware.com>
2025-02-25 09:59:50 -0500

    add changeset

---
+++ src/main/groovy/com/spidasoftware/schema/conversion/changeset/v12/ComponentBraceChangeset.groovy
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2025 Bentley Systems, Incorporated. All rights reserved.
+ */
+package com.spidasoftware.schema.conversion.changeset.v12
+
+import com.spidasoftware.schema.conversion.changeset.ConversionException
+import com.spidasoftware.schema.conversion.changeset.client.AbstractClientDataChangeSet
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class ComponentBraceChangeset extends AbstractClientDataChangeSet {
+
+	@Override
+	boolean applyToClientData(Map clientDataJSON) throws ConversionException {
+		// do nothing
+		return false
+	}
+
+	@Override
+	boolean revertClientData(Map clientDataJSON) throws ConversionException {
+		boolean anyChanged = false
+		if(clientDataJSON.containsKey("componentBraces")) {
+			anyChanged = !(clientDataJSON.componentBraces as List).isEmpty()
+			clientDataJSON.remove("componentBraces")
+		}
+
+		clientDataJSON.assemblies?.each { Map assembly ->
+			anyChanged |= revertStructure(assembly.assemblyStructure as Map)
+		}
+		return anyChanged
+	}
+
+	@Override
+	void revertDesign(Map designJSON) throws ConversionException {
+		super.revertDesign(designJSON)
+
+		if (designJSON.structure != null) {
+			revertStructure(designJSON.structure as Map)
+		}
+	}
+
+	@Override
+	boolean revertResults(Map resultsJSON) throws ConversionException {
+		super.revertResults(resultsJSON)
+
+		if (resultsJSON.analyzedStructure != null) {
+			return revertStructure(resultsJSON.analyzedStructure as Map)
+		}
+		return false
+	}
+
+	protected boolean revertStructure(Map structureJSON) {
+		return false  // todo AB#1614910
+	}
+}
