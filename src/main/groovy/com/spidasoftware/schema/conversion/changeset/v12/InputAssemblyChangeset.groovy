daad9ec9504da74e3bf334c491739db451adc79e
Phillip Lai <Phillip.Lai@bentley.com>
2025-05-14 11:49:30 -0700

    no tests yet

---
+++ src/main/groovy/com/spidasoftware/schema/conversion/changeset/v12/InputAssemblyChangeset.groovy
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2025 Bentley Systems, Incorporated. All rights reserved.
+ */
+package com.spidasoftware.schema.conversion.changeset.v12
+
+import com.spidasoftware.schema.conversion.changeset.ConversionException
+import com.spidasoftware.schema.conversion.changeset.calc.AbstractCalcDesignChangeset
+
+class InputAssemblyChangeset extends AbstractCalcDesignChangeset {
+
+	@Override
+	void applyToDesign(Map designJSON) throws ConversionException {
+		// do nothing
+	}
+
+	@Override
+	void revertDesign(Map designJSON) throws ConversionException {
+		def assemblies = designJSON.structure?.assemblies
+		assemblies?.each { assembly ->
+			assembly.wireEndPoints?.each { wep ->
+				wep.remove("clientDataWEPid")
+			}
+		}
+	}
+
+}
---
+++ src/test/groovy/com/spidasoftware/schema/conversion/changeset/v12/InputAssemblyChangesetTest.groovy
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2025 Bentley Systems, Incorporated. All rights reserved.
+ */
+package com.spidasoftware.schema.conversion.changeset.v12
+
+import spock.lang.Specification
+
+class InputAssemblyChangesetTest extends Specification {
+
+	static InputAssemblyChangeset changeset
+
+	def setupSpec() {
+		changeset = new InputAssemblyChangeset()
+	}
+
+	// make tests later
+}
