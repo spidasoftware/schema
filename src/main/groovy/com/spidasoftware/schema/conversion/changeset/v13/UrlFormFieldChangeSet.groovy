00fa6b5c5000c83b0a0a73fc379e8216160ce47d
Jason Garrett <jason.garrett@bentley.com>
2026-06-09 16:56:43 -0400

    changeset

---
+++ src/main/groovy/com/spidasoftware/schema/conversion/changeset/v13/UrlFormFieldChangeSet.groovy
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2026 Bentley Systems, Incorporated. All rights reserved.
+ */
+package com.spidasoftware.schema.conversion.changeset.v13
+
+import com.spidasoftware.schema.conversion.changeset.ConversionException
+import com.spidasoftware.schema.conversion.changeset.client.AbstractClientDataChangeSet
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class UrlFormFieldChangeSet extends AbstractClientDataChangeSet {
+
+    @Override
+    boolean applyToClientData(Map clientDataJSON) throws ConversionException {
+        // do nothing
+        return false
+    }
+
+    @Override
+    boolean revertClientData(Map clientDataJSON) throws ConversionException {
+        boolean changed = false
+
+        clientDataJSON.locationForms?.each { Map form ->
+            changed |= revertForm(form)
+        }
+        clientDataJSON.projectForms?.each { Map form ->
+            changed |= revertForm(form)
+        }
+
+        return changed
+    }
+
+    @Override
+    void revertProject(Map projectJSON) throws ConversionException {
+        super.revertProject(projectJSON)
+
+        projectJSON.forms?.each { Map form ->
+            revertForm(form)
+        }
+        projectJSON.defaultLocationForms?.each { Map form ->
+            revertForm(form)
+        }
+    }
+
+    @Override
+    void revertLocation(Map locationJSON) throws ConversionException {
+        super.revertLocation(locationJSON)
+        locationJSON.forms?.each { Map form ->
+            revertForm(form)
+        }
+    }
+
+    protected boolean revertForm(Map form) {
+        boolean changed = false
+
+        List<Map> formDefinition = form.formDefinition as List<Map>
+
+        // simple fields
+        List<String> urlFieldNames = revertDefinition(formDefinition)
+        revertFields(form.fields as Map<String, ?>, urlFieldNames)
+        changed |= !urlFieldNames.isEmpty()
+
+        // tab containers
+        List<Map> tabFields = formDefinition.findAll { it.fieldType == "customTabbedPane" }
+        tabFields.each { Map tabField ->
+            urlFieldNames = revertDefinition(tabField.fieldDefinitions as List<Map>)
+            revertFields(form.fields[tabField.fieldName as String] as Map<String, ?>, urlFieldNames)
+            changed |= !urlFieldNames.isEmpty()
+        }
+
+        // table containers
+        List<Map> tableFields = formDefinition.findAll { it.fieldType == "customTable" }
+        tableFields.each { Map tableField ->
+            urlFieldNames = revertDefinition(tableField.fieldDefinitions as List<Map>)
+            (form.fields[tableField.fieldName as String] as List<Map<String, String>>).each { Map<String, ?> row ->
+                revertFields(row, urlFieldNames)
+            }
+            changed |= !urlFieldNames.isEmpty()
+        }
+
+        return changed
+    }
+
+    /**
+     * Remove any url fields from the form definition, and return a list of the field names of the removed url fields.
+     */
+    protected List<String> revertDefinition(List<Map<String, String>> definition) {
+        List<Map> fieldsToRevert = definition.findAll { it.fieldType == "customUrlField" }
+        fieldsToRevert.each { definition.remove(it) }
+        return fieldsToRevert*.fieldName
+    }
+
+    /**
+     * Remove any fields with the given field names from the given fields map.
+     */
+    protected void revertFields(Map<String, ?> fields, List<String> fieldNamesToRevert) {
+        fieldNamesToRevert.each { String fieldName ->
+            fields.removeAll {it.key == fieldName }
+        }
+    }
+}
